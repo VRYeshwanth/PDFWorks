56e398ac79da482ea3ef6c3f2efa810f1b193c39
Yeshwanth <vryeshwanth06@gmail.com>
2025-12-26 17:08:43 +0530

    Implement CORS

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.merge import router as merge_router
 
 app = FastAPI(title="PDFWorks Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(merge_router)
 
